37e2c63f611b9cf32481f8879ca16fe3e8fa115f
ACER <ledinhduythuc510@gmail.com>
2023-11-25 18:11:28 +0700

    navbar user and admin

--- Page/AdminNavbar.php
+++ Page/AdminNavbar.php
@@ -10,20 +10,11 @@
 
 <nav class="navbarr">
     <ul>
-        <li><a href="home.php">Home</a></li>
-        <li class="dropdownn">
-            <a href="../Page/Products.php" class="dropbtnn">Product</a>
-            <div class="dropdownn-content">
-                <a href="../Page/all_products.php">Shoes</a>
-                <a href="#">Shirt</a>
-                <a href="#">Short</a>
-            </div>
-        </li>
-        <li><a href="#">Cart</a></li>
+        <li><a href="admin_dashboard.php">Dashboard</a></li>
         <li class="dropdownn" style="float: right;"> 
             <a href="#" class="dropbtnn" id="profile">View Profile</a>
             <div class="dropdownn-content">
-                <a href="../Page/user_page.php">My Profile</a>
+                <a href="../Page/admin_page.php">My Profile</a>
             </div>
         </li>
     </ul>
