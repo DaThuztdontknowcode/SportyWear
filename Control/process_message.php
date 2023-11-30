da56d2c30d9c920373219a5270acf58e22e85361
dangthanh03 <dangtrithanh03@gmail.com>
2023-11-30 23:46:17 +0700

    nothing

--- Control/process_message.php
+++ Control/process_message.php
@@ -1,20 +1,45 @@
 <?php
-// Trong một tệp xử lý nhắn tin, ví dụ: process_message.php
+// Trong một tệp xử lý nhắn tin và hiển thị tin nhắn, ví dụ: messages.php
 
 // Kết nối đến cơ sở dữ liệu
 require_once '../config.php';
 
-// Lấy dữ liệu từ biểu mẫu gửi tin nhắn
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $message = $_POST['message'];
-    // Có thể lấy thông tin người gửi từ phiên đăng nhập hoặc thông tin khách hàng
+// Bắt đầu phiên làm việc
+session_start();
 
-    // Thực hiện thêm tin nhắn vào cơ sở dữ liệu
-    $sql = "INSERT INTO messages (user_id, message_content) VALUES (1, '$message')";
-    if ($conn->query($sql) === TRUE) {
-        echo json_encode(['status' => 'success', 'message' => 'Tin nhắn đã được gửi thành công']);
-    } else {
-        echo json_encode(['status' => 'error', 'message' => 'Lỗi khi gửi tin nhắn: ' . $conn->error]);
+// Lấy thông tin người dùng từ phiên làm việc (đã đăng nhập)
+if (isset($_SESSION['user_id'])) {
+    $userId = $_SESSION['user_id'];
+
+    // Xử lý việc gửi tin nhắn
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $message = $_POST['message'];
+
+        // Thực hiện thêm tin nhắn vào cơ sở dữ liệu
+        $sql = "INSERT INTO messages (user_id, message_content) VALUES ('$userId', '$message')";
+        if ($conn->query($sql) === TRUE) {
+            echo json_encode(['status' => 'success', 'message' => 'Tin nhắn đã được gửi thành công']);
+        } else {
+            echo json_encode(['status' => 'error', 'message' => 'Lỗi khi gửi tin nhắn: ' . $conn->error]);
+        }
     }
+
+    // Lấy tin nhắn từ cơ sở dữ liệu dựa trên ID người dùng
+    $sql = "SELECT * FROM messages WHERE user_id = '$userId'";
+    $result = $conn->query($sql);
+
+    // Kiểm tra và hiển thị tin nhắn
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo '<p>' . $row['message_content'] . '</p>';
+        }
+    } else {
+        echo '<p>No messages found.</p>';
+    }
+} else {
+    echo json_encode(['status' => 'error', 'message' => 'Người dùng chưa đăng nhập']);
 }
+
+// Đóng kết nối cơ sở dữ liệu
+$conn->close();
 ?>
--- Page/Products.php
+++ Page/Products.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Slider</title>
@@ -9,6 +10,8 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+
+
 </head>
 
 <body>
