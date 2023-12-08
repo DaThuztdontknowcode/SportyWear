0b9be1d52631dbe2a7ab68fc33738e4940cb5935
ACER <ledinhduythuc510@gmail.com>
2023-12-08 20:50:52 +0700

    abc

--- Page/home.php
+++ Page/home.php
@@ -21,7 +21,7 @@
 <header>
     <div class="container-fluid p-0">
         <!-- Background image or slideshow here -->
-        <div class="header-background" style="background-image: url('https://i.pinimg.com/564x/d0/49/1a/d0491acbb51bb1489f3df3953ecb96e5.jpg');">
+        <div class="header-background" style="background-image: url('https://i.pinimg.com/736x/87/49/15/8749155a4b1180243f283384e55981de.jpg');">
             <!-- Overlay with Opacity to darken the image for text visibility -->
             <div class="header-overlay" style="background: rgba(0, 0, 0, 0.5);">
                 <div class="container text-center">
@@ -182,29 +182,32 @@
 
 <!-- Thêm sau section-2 -->
 <section id="section-3" class="section-3 container-fluid p-0">
-    <div class="section-title text-center">
+    <div class="section-title">
         <h2>Lebron Collection</h2>
     </div>
-    
-    <div class="row no-gutters">
-        <div class="col-12 col-sm-6 col-md-6 image-column text-center">
-            <img class="img-fluid" src="https://i.pinimg.com/564x/ec/2e/34/ec2e34586df8da3b918f9ef882e890ff.jpg" alt="Image 1">
-            <button class="btn explore-btn mt-2">Explore Now</button>
+    <div class="image-row">
+        <div class="image-column">
+            <img src="https://i.pinimg.com/564x/ec/2e/34/ec2e34586df8da3b918f9ef882e890ff.jpg" alt="Image 1">
+            <button class="explore-btn">Explore Now</button>
+            <img id="follow-img" src="https://images.footballfanatics.com/nike-white-nba-headband_pi2694000_ff_2694897_full.jpg?_hv=2" alt="Follow Image" style="display: none; position: absolute;heigh: 300px;width:300px;border-radius: 5px;z-index: 999;"/>
         </div>
         
-        <div class="col-12 col-sm-6 col-md-6 image-column text-center">
-            <img class="img-fluid" src="https://i.pinimg.com/564x/b1/c9/b1/b1c9b11586ded034794cf2a7f6e86f0f.jpg" alt="Image 2">
-            <button class="btn explore-btn mt-2" onclick="redirectToProduct44()">Explore Now</button>
+        <div class="image-column">
+            <img src="https://i.pinimg.com/564x/b1/c9/b1/b1c9b11586ded034794cf2a7f6e86f0f.jpg" alt="Image 2">
+            <button class="explore-btn" onclick="redirectToProduct44()">Explore Now</button>
+            <img id="follow-img2" src="https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/539492/01/mod01/fnd/PNA/fmt/png/PUMA-x-TMC-Everyday-Hussle-Men's-Sweatshorts" alt="Follow Image" style="display: none; position: absolute;heigh: 300px;width:300px;border-radius: 5px;z-index: 999;"/>
         </div>
 
-        <div class="col-12 col-sm-6 col-md-6 image-column text-center">
-            <img class="img-fluid" src="https://i.pinimg.com/564x/93/c7/bd/93c7bd60a0941c5a68591b6d74788cc7.jpg" alt="Image 3">
-            <button class="btn explore-btn mt-2">Explore Now</button>
+        <div class="image-column">
+            <img src="https://i.pinimg.com/564x/93/c7/bd/93c7bd60a0941c5a68591b6d74788cc7.jpg" alt="Image 3">
+            <button class="explore-btn">Explore Now</button>
+            <img id="follow-img3" src="https://images.footballfanatics.com/nike-white-nba-headband_pi2694000_ff_2694897_full.jpg?_hv=2" alt="Follow Image" style="display: none; position: absolute;heigh: 300px;width:300px;border-radius: 5px;z-index: 999;"/>
         </div>
 
-        <div class="col-12 col-sm-6 col-md-6 image-column text-center">
-            <img class="img-fluid" src="https://i.pinimg.com/564x/c0/b2/02/c0b202cd2463bff530fdb3d091c5e4e1.jpg" alt="Image 4">
-            <button class="btn explore-btn mt-2">Explore Now</button>
+        <div class="image-column">
+            <img src="https://i.pinimg.com/564x/c0/b2/02/c0b202cd2463bff530fdb3d091c5e4e1.jpg" alt="Image 4">
+            <button class="explore-btn">Explore Now</button>
+            <img id="follow-img4" src="https://images.footballfanatics.com/nike-white-nba-headband_pi2694000_ff_2694897_full.jpg?_hv=2" alt="Follow Image" style="display: none; position: absolute;heigh: 300px;width:300px;border-radius: 5px;z-index: 999;"/>
         </div>
     </div>
 </section>
--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'sportwear');
+$conn = mysqli_connect('localhost', 'root', '', 'sportwearfinal');
 ?>
